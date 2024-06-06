aee28c5be11de1849a8674a1a790ff5cecdc3748
NoWaaLT <88542659+NoWaaLT@users.noreply.github.com>
2024-06-07 01:17:27 +0300

    Spread list of tags
    Spread list of tags

--- src/main/java/com/orioninc/fromweb/SearchFromWeb.java
+++ src/main/java/com/orioninc/fromweb/SearchFromWeb.java
@@ -14,58 +14,58 @@ import java.io.*;
 import static com.orioninc.logger.LogUtil.logWarning;
 
 public class SearchFromWeb implements Search {
-  private String url;
+    private String url;
 
-  public SearchFromWeb(String url) {
-    this.url = url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
-
-  @Override
-  public void search() {
-
-    String extractedFileName = getExtractedFileName(getElementsFromWeb());
-
-    FillTheBinarySearchTree fillTheBinarySearchTree =
-            new FillTheBinarySearchTree(extractedFileName, REGEX);
-
-    BinarySearchTree bst = fillTheBinarySearchTree.returnBinaryTreeFromFile();
-    String specificWord = inputWord();
-    boolean searchStatus = bst.search(specificWord);
-    int source = 1; // Means from WebPage
-    printResultAndPushToDatabase(searchStatus, bst, specificWord, source);
-  }
-
-  public Elements getElementsFromWeb() {
-    Elements elements;
-    try {
-      Document document = Jsoup.connect(this.url).get();
-      elements = document.select("h1, h2, h3, h4, h5, h6, p, a, li, span, small, button");
-    } catch (IOException e) {
-      logWarning("File is not found.");
-      elements = null;
+    public SearchFromWeb(String url) {
+        this.url = url;
     }
 
-    return elements;
-  }
-
-  public String getExtractedFileName(Elements elements) {
-    String fileName = "";
-
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter("extracted.txt"))) {
-      for (Element element : elements) {
-        writer.write(element.text());
-        writer.newLine();
-        fileName = "extracted.txt";
-      }
-    } catch (IOException e) {
-      logWarning("Something went wrong.");
-      fileName = null;
+    public void setUrl(String url) {
+        this.url = url;
     }
-    return fileName;
-  }
+
+    @Override
+    public void search() {
+
+        String extractedFileName = getExtractedFileName(getElementsFromWeb());
+
+        FillTheBinarySearchTree fillTheBinarySearchTree =
+                new FillTheBinarySearchTree(extractedFileName, REGEX);
+
+        BinarySearchTree bst = fillTheBinarySearchTree.returnBinaryTreeFromFile();
+        String specificWord = inputWord();
+        boolean searchStatus = bst.search(specificWord);
+        int source = 1; // Means from WebPage
+        printResultAndPushToDatabase(searchStatus, bst, specificWord, source);
+    }
+
+    public Elements getElementsFromWeb() {
+        Elements elements;
+        try {
+            Document document = Jsoup.connect(this.url).get();
+            elements = document.select("h1, h2, h3, h4, h5, h6, p, a, li, span, small, button, div, b, i, u, em, strong, blockquote, pre, code, address, cite, q, ins, del, mark, abbr, acronym, sub, sup, kbd, samp, var, time, tt, output, summary, label, legend, caption, th, td, dt, dd");
+        } catch (IOException e) {
+            logWarning("File is not found.");
+            elements = null;
+        }
+
+        return elements;
+    }
+
+    public String getExtractedFileName(Elements elements) {
+        String fileName = "";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("extracted.txt"))) {
+            for (Element element : elements) {
+                writer.write(element.text());
+                writer.newLine();
+                fileName = "extracted.txt";
+            }
+        } catch (IOException e) {
+            logWarning("Something went wrong.");
+            fileName = null;
+        }
+        return fileName;
+    }
 
 }
